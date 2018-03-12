becf569c8d60cf834e7eddf7548082ba9b965dff
Jürgen Ratzenböck <juergen.ratzenboeck@gmail.com>
2018-03-12 22:37:38 +0100

    Add page to show details of meal

--- app/routes/meal.js
+++ app/routes/meal.js
@@ -1,7 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  model() {
-
+  model(params) {
+    return this.get('store').findRecord('meal', params.meal_id);
   }
 });
